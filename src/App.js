012f6744820800ddc97efca86f0dae32429b2ba9
3ndrius <andrzejo1991@gmail.com>
2018-04-30 17:22:59 +0200

    Fix and desc

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+// own imports
 import Titles from './components/Titles';
 import Weather from './components/Weather';
 import Form from './components/Form';
@@ -11,13 +12,20 @@ const API_KEY = "b6907d289e10d714a6e88b30761fae22";
 
 class App extends Component {
 
+  state = {
+
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
     
-  }
+  }// end of getWeat func
  
   render() {
 
