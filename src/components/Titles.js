df1e6e37301748bbe715f860a9193cb52a8b46ed
3ndrius <andrzejo1991@gmail.com>
2018-04-30 14:21:59 +0200

    Title component

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Titles extends Component {
+
+    render() {
+        return(
+            <h1> Weather Finder</h1>
+            <p> Find out temperature in this location </p>
+        );
+    }
+};
+
+export default Titles;
