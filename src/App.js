96b22a133a3e478159a92b10024cb8830e011a81
3ndrius <andrzejo1991@gmail.com>
2018-04-30 16:48:37 +0200

    Weather and form component start

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Titles from './components/Titles';
+import Weather from './components/Weather';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        
-       <Titles/>
+       <Titles />
+       <Form />
+       <Weather />
       </div>
     );
   }
