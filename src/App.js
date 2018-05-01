992defc3317493d18e5c1a5180c5977195b8e43f
3ndrius <andrzejo1991@gmail.com>
2018-05-01 11:49:03 +0200

    Fetching data from weather api

--- src/App.js
+++ src/App.js
@@ -23,22 +23,36 @@ class App extends Component {
 
   getWeather = async (e) => {
     e.preventDefault();
+  
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
-    const data = await api_call.json();
-    console.log(data);
-    
-    this.setState( {
-      temperature:data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.humidity,
-      description: data.weather[0].description,
-      error: ""
+    if(city && country) {
 
-    }); //end setstate
+      const api_call = await fetch(`http://openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+      const data = await api_call.json();
+      console.log(data);
+      this.setState( {
+        temperature:data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.humidity,
+        description: data.weather[0].description,
+        error: ""
+  
+      }); //end setstate
+    }//end if
+    else{
+      this.setState({
+        temperature:undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please type your location properly!"
+  
+      }); //end setstate
+    }//end else   
 
   }// end of getWeat func
  
@@ -55,7 +69,8 @@ class App extends Component {
                 humidity={this.state.humidity}
                 description={this.state.description}
                 error={this.state.error}
-                            />
+        />
+         <h1>{this.state.error} </h1>
       </div>
     );
   }//end render
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,12 @@ class Weather extends React.Component {
 
         return (
             <div> 
-                <p>{this.props.temperature} </p>
+               
+                {this.props.city && this.props.country && <p>Location: {this.props.city} | { this.props.country} </p>}
+                {this.props.temperature && <p>Temp: {this.props.temperature} </p>}
+                {this.props.humidity && <p>Humidity: {this.props.himidity} </p>}
+                {this.props.description && <p> Condition: {this.props.description} </p> }
+                <p>{this.props.error} </p>
             </div>
         );
     }
