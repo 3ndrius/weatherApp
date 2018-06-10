845fc2f45267884a371a72d45b992fcaafa12e86
3ndrius <3ndrius@users.noreply.github.com>
2018-06-10 20:51:03 +0200

    Update Titles.js

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-
 const Titles = () => (
-
         <div>
         <h1> Weather Finder</h1>
         <p> Find out temperature in your location </p>
         </div>
     );
-
-
-
 export default Titles;
