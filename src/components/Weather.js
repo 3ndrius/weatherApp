4f32cfd68a77c04ac80e6175c522453ca53c30c1
3ndrius <3ndrius@users.noreply.github.com>
2018-06-10 20:51:55 +0200

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,6 @@
 import React from 'react';
-
 const Weather = props => (
-
     <div> 
-
         {props.city && props.country && <p>Location: {props.city} | { props.country} </p>}
         {props.temperature && <p>Temp: {props.temperature} °C </p>}
         {props.minTemp && props.maxTemp && <p>MinMaxTemp: {props.minTemp} °C  | {props.maxTemp} °C  </p>}
@@ -11,14 +8,8 @@ const Weather = props => (
         {props.pressure && <p>Pressure: {props.pressure} hPa </p>}
         {props.description && <p> Condition: {props.description} </p> }
         {props.wind && <p> Wind: {props.wind} km/h</p> }
-        {props.icon && <span className="icon"><img src={`http://openweathermap.org/img/w/${props.icon}.png`} alt="weather icon" /> </span>  }
-        
+        {props.icon && <span className="icon"><img src={`http://openweathermap.org/img/w/${props.icon}.png`} alt="weather icon" /> </span>  } 
         {props.error && <p>{props.error} </p>}
-
     </div>
 );
-
-   
-
-
 export default Weather;
