f11095f5a9e0a15de788aacceb4fae65b0d69f0b
3ndrius <3ndrius@users.noreply.github.com>
2018-08-28 19:33:02 +0200

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -7,7 +7,6 @@
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -17,7 +16,6 @@ const isLocalhost = Boolean(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
-
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
@@ -28,7 +26,6 @@ export default function register() {
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
-
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
@@ -51,7 +48,6 @@ export default function register() {
     });
   }
 }
-
 function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
@@ -80,7 +76,6 @@ function registerValidSW(swUrl) {
       console.error('Error during service worker registration:', error);
     });
 }
-
 function checkValidServiceWorker(swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
@@ -107,7 +102,6 @@ function checkValidServiceWorker(swUrl) {
       );
     });
 }
-
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
